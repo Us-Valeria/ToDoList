bf9f076439bba5aa74770d3149a0258244d54f7e
Валерия Гранина <valeria.granina04@gmail.com>
2023-04-23 22:37:32 +0700

    Add changeStatus

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,84 +1,108 @@
-import React, { useState /* useEffect */ } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 let nextId = 0;
 
 function App() {
   const [item, setItem] = useState('');
-  let [items, setItems] = useState([]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('items')));
+  let [filter, setFilter] = useState('all');
 
-  //let [Filter, setFilter] = useState('all');
-  /*let [items, setItems] = useState(
-    JSON.parse(localStorage.getItem('items')) 
-  );*/
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  });
 
-  /*useEffect(() => {
-    localStorage.setItem('items', JSON.stringify('items'));
-  }, [items]);*/
-
-  const newItem = () => {
+  const addItem = (item) => {
+    if (item === '') return;
     const newItem = {
       id: nextId++,
       todo: item,
-      status: false,
+      completed: false,
     };
-    setItems((items) => [...items, newItem]);
-    setItem('');
+    let newItems = [...items, newItem];
+    setItems(newItems);
   };
 
-  /*const changeFilter = () => {
-    let filterItem = items;
-    if (Filter === 'active') {
-      filterItem = items.filter((t) => t.status === false);
+  const changeFilter = (status) => {
+    if (status === 'active') {
+      filter = items.filter((t) => t.completed === false);
     }
-
-    if (Filter === 'complete') {
-      filterItem = items.filter((t) => t.status === true);
+    if (status === 'completed') {
+      filter = items.filter((t) => t.completed === true);
     }
-    setFilter(filterItem);
-  };*/
-
-  const changeStatus = (Status) => {
-    if (Status) {
-      Status = false;
-    } else {
-      Status = true;
-    }
+    setFilter(filter);
   };
 
   const removeItem = (id) => {
-    items = items.filter((t) => t.id !== id);
-    setItems(items);
+    setItems(items.filter((t) => t.id != id));
+  };
+
+  const changeStatusItem = (id, completed) => {
+    if (items.find((t) => t.id === id)) item.completed = !completed;
+    setItems([...items]);
   };
 
   return (
-    <div className="App">
+    <div className="app">
       <h1>Список задач: </h1>
-      <div className="Input_Field">
+      <div className="input-field">
         <input
-          className="Field"
+          className="field"
           type="text"
           placeholder="Введите задачу..."
+          value={item}
           onChange={(e) => {
             setItem(e.target.value);
           }}
+          onKeyPress={(e) => {
+            if (e.charCode === 13) {
+              addItem(item);
+              setItem('');
+            }
+          }}
         />
-        <button className="Button_Save" onClick={() => newItem()}>
+        <button
+          className="button-save"
+          onClick={() => {
+            addItem(item);
+            setItem('');
+          }}
+        >
           Добавить
         </button>
         <div>
-          <button className="Buttons">Все</button>
-          <button className="Buttons">Текущие</button>
-          <button className="Buttons">Завершенные</button>
+          <button
+            className="buttons"
+            type="radio"
+            onClick={() => changeFilter('all')}
+          >
+            Все
+          </button>
+          <button
+            className="buttons"
+            type="radio"
+            onClick={() => changeFilter('active')}
+          >
+            Текущие
+          </button>
+          <button
+            className="buttons"
+            type="radio"
+            onClick={() => changeFilter('completed')}
+          >
+            Завершенные
+          </button>
         </div>
         <ul>
-          <div className="Item">
+          <div className="item">
             {items.map((items) => (
               <li key={items.id}>
                 <input
                   type="checkbox"
-                  checked={items.status}
-                  onClick={changeStatus(items.status)}
+                  checked={item.completed}
+                  onChange={(e) =>
+                    changeStatusItem(item.id, e.currentTarget.checked)
+                  }
                 />
                 <span>{items.todo}</span>
                 <svg
@@ -93,7 +117,13 @@ function App() {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107
+                     1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 
+                     2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 
+                     48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 
+                     0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964
+                      51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 
+                      0a48.667 48.667 0 00-7.5 0"
                   />
                 </svg>
               </li>
